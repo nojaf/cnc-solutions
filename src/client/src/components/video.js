a7de32e83df6d3fc3ccf77be89353d28b7a852b3
nojaf <florian.verdonck@outlook.com>
2023-06-03 09:57:37 +0200

    Revert "Verify id is ok."
    This reverts commit 586034007f736958d62bae414352961088e64008.

--- src/client/src/components/video.js
+++ src/client/src/components/video.js
@@ -1,64 +1,61 @@
-import React, {useRef, useState, useEffect} from "react"
+import React, { useRef, useState, useEffect } from "react"
 
 const Video = ({
-                   videoId,
-                   altText,
-                   width = 640,
-                   height = 360,
-                   mute = true,
-                   controls = false,
-                   autoplay = true,
-               }) => {
-    const toInt = (b) => (b ? 1 : 0)
-    const url = `https://www.youtube.com/embed/${videoId}?autoplay=${toInt(
-        autoplay
-    )}&loop=1&rel=0&modestbranding=1&controls=${toInt(
-        controls
-    )}&showinfo=0&origin=https://cncsolutions.be&playlist=${videoId}&mute=${toInt(
-        mute
-    )}`
-    const video = useRef(null)
-    const [videoHeight, setVideoHeight] = useState(175)
+  videoId,
+  altText,
+  width = 640,
+  height = 360,
+  mute = true,
+  controls = false,
+  autoplay = true,
+}) => {
+  const toInt = (b) => (b ? 1 : 0)
+  const url = `https://www.youtube.com/embed/${videoId}?autoplay=${toInt(
+    autoplay
+  )}&loop=1&rel=0&modestbranding=1&controls=${toInt(
+    controls
+  )}&showinfo=0&origin=https://cncsolutions.be&playlist=${videoId}&mute=${toInt(
+    mute
+  )}`
+  const video = useRef(null)
+  const [videoHeight, setVideoHeight] = useState(175)
 
-    useEffect(() => {
-        const resizeListener = () => {
-            if (video.current) {
-                setVideoHeight((video.current.clientWidth / 16) * 9)
-            }
-        }
+  useEffect(() => {
+    const resizeListener = () => {
+      if (video.current) {
+        setVideoHeight((video.current.clientWidth / 16) * 9)
+      }
+    }
+    // set resize listener
+    window.addEventListener("resize", resizeListener)
 
-        // Set resize listener
-        window.addEventListener("resize", resizeListener)
+    if (video.current) {
+      setVideoHeight((video.current.clientWidth / 16) * 9)
+    }
 
-        // Check if video ref is defined
-        if (video.current) {
-            setVideoHeight((video.current.clientWidth / 16) * 9)
-        }
+    // clean up function
+    return () => {
+      // remove resize listener
+      window.removeEventListener("resize", resizeListener)
+    }
+  }, [video])
 
-        // Clean up function
-        return () => {
-            // Remove resize listener
-            window.removeEventListener("resize", resizeListener)
-        }
-    }, [video])
-
-    return (
-        <div className="video">
-            {videoId &&
-                <iframe
-                    src={url}
-                    title={altText}
-                    height={videoHeight}
-                    width={"100%"}
-                    ref={video}
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    frameBorder="0"
-                    webkitallowfullscreen="true"
-                    mozallowfullscreen="true"
-                    allowFullScreen
-                />}
-        </div>
-    )
+  return (
+    <div className="video">
+      <iframe
+        src={url}
+        title={altText}
+        height={videoHeight}
+        width={"100%"}
+        ref={video}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        frameBorder="0"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        allowFullScreen
+      />
+    </div>
+  )
 }
 
 export default Video
