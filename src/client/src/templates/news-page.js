0c00dd89c17437b660fb30532f7dcbe18ba8cd14
nojaf <florian.verdonck@outlook.com>
2025-09-10 21:17:24 +0200

    Initial news detail page

--- src/client/src/templates/news-page.js
+++ src/client/src/templates/news-page.js
@@ -5,9 +5,50 @@ import { pageInCulture, useUrl } from "../selectors"
 import Header from "../components/header"
 import PageIntroduction from "../components/pageIntroduction"
 
+const TextBlock = ({ content }) => {
+  return (
+    <div
+      className="news-text"
+      dangerouslySetInnerHTML={{ __html: content }}
+    ></div>
+  )
+}
+
+const ImageBlock = ({ image, altText, caption, isFullWidth }) => {
+  const imgTag = (
+    <img
+      className={isFullWidth ? "full-width" : ""}
+      src={image}
+      alt={altText}
+    />
+  )
+  return (
+    <div className="news-image">
+      {caption ? (
+        <figure>
+          {imgTag}
+          <figcaption>{caption}</figcaption>
+        </figure>
+      ) : (
+        imgTag
+      )}
+    </div>
+  )
+}
+
 const NewsPage = ({ data, pageContext }) => {
   const currentCulture = pageContext.culture
   const newsPage = pageInCulture(currentCulture, data.newsPage)
+  const texts = data.texts.nodes.map((node) =>
+    pageInCulture(currentCulture, node)
+  )
+  const images = data.images.nodes.map((node) =>
+    pageInCulture(currentCulture, node)
+  )
+  const blocks = [...texts, ...images].toSorted(
+    (a, b) => a.sortOrder - b.sortOrder || a.key - b.key
+  )
+  console.log(blocks)
   return (
     <Layout
       culture={currentCulture}
@@ -16,6 +57,15 @@ const NewsPage = ({ data, pageContext }) => {
     >
       <Header currentPage={newsPage} />
       <PageIntroduction {...newsPage} />
+      <div className="container">
+        {blocks.map((block) =>
+          block.kind === "newsText" ? (
+            <TextBlock {...block} />
+          ) : (
+            <ImageBlock {...block} />
+          )
+        )}
+      </div>
     </Layout>
   )
 }
@@ -69,6 +119,45 @@ export const query = graphql`
         nl
         en
         fr
+      }
+    }
+    texts: allNewsText(filter: { parentUmbracoId: { eq: $umbracoId } }) {
+      nodes {
+        key: umbracoId
+        kind: __typename
+        sortOrder
+        content {
+          nl
+          en
+          fr
+        }
+      }
+    }
+    images: allNewsImage(filter: { parentUmbracoId: { eq: $umbracoId } }) {
+      nodes {
+        key: umbracoId
+        kind: __typename
+        sortOrder
+        image {
+          nl
+          en
+          fr
+        }
+        altText {
+          nl
+          fr
+          en
+        }
+        caption {
+          nl
+          fr
+          en
+        }
+        isFullWidth {
+          nl
+          fr
+          en
+        }
       }
     }
   }
