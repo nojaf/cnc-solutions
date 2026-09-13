00974bb1e961f320c11e02d044089ea6f2bc5e48
nojaf <florian.verdonck@outlook.com>
2026-09-13 10:35:20 +0200

    Add sitema and robots.txt

--- src/client/astro.config.mjs
+++ src/client/astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 import umbracoSignalR from "./src/integrations/umbraco-signalr";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://cncsolutions.be",
   image: {
     domains: ["cncsolutions-backend.azurewebsites.net"],
   },
-  integrations: [umbracoSignalR()],
+  integrations: [sitemap(), umbracoSignalR()],
   vite: {
     plugins: [tailwindcss()],
   },
